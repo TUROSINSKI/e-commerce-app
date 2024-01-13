2f42b9dc07f1a520ddc78f7983ebf8cdfb1509a7
TUROSINSKI <turosinskipriv@gmail.com>
2024-01-13 16:13:49 +0100

    Added Provider and reducer, checkout component

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,22 @@ import React from "react";
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className="header">
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+            <Link to='/'>
+                <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+            </Link>
 
             <div className="header__search">
                 <input className="header__searchInput" type="text" />
-                <SearchIcon className="header__searchIcon"/>
+                <SearchIcon className="header__searchIcon" />
             </div>
 
             <div className="header__nav">
@@ -20,14 +27,16 @@ function Header() {
                 </div>
 
                 <div className="header__option">
-                <span className="header__optionLineOne">Welcome back</span>
+                    <span className="header__optionLineOne">Welcome back</span>
                     <span className="header__optionLineTwo">Sign In</span>
                 </div>
 
-                <div className="header__optionBasket">
-                    <ShoppingBagIcon />
-                    <span className="header__optionLineTwo header__basketCount">0</span>
-                </div>
+                <Link to='/checkout' className="header__optionBasket">
+                    <div className="header__optionBasket">
+                        <ShoppingBagIcon />
+                        <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
+                    </div>
+                </Link>
             </div>
         </div>
     )
