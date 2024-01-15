88ad6803279507ad176c8d5575b72a7221d325fb
TUROSINSKI <turosinskipriv@gmail.com>
2024-01-15 18:01:42 +0100

    SignUp SignIn working

--- client/src/SignIn.js
+++ client/src/SignIn.js
@@ -1,24 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate } from 'react-router-dom';
 import './SignIn.css'
 
 function SignIn() {
+
+    const [email, setEmail] = useState('');
+    const [haslo, setHaslo] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch('http://localhost:5000/api/loginUser', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ Email: email, Haslo: haslo }),
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+    
+            const result = await response.json();
+
+            console.log(result);
+            navigate('/');
+
+    
+        } catch (error) {
+            console.error("Error during authentication", error);
+
+        }
+    };
+
     return (
         <div className="signIn">
             <div className="signIn__container">
                 <h1>Sign In</h1>
                 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h5>Email</h5>
-                    <input type="text" />
-
+                    <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+    
                     <h5>Password</h5>
-                    <input type="password" />
-
-                    <button className="signIn__button">Sign In</button>
+                    <input type="password" value={haslo} onChange={(e) => setHaslo(e.target.value)} />
+    
+                    <button type="submit" className="signIn__button">Sign In</button>
                 </form>
             </div>
         </div>
-    )
+    );
 }
 
 export default SignIn
--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -1,23 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 import './SignUp.css'
 
 function SignUp() {
+
+    const [Imie, setImie] = useState('');
+    const [Nazwisko, setNazwisko] = useState('');
+    const [Email, setEmail] = useState('');
+    const [Haslo, setHaslo] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch("http://localhost:5000/api/registerUser", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ Imie, Nazwisko, Email, Haslo }),
+            });
+
+            console.log(response)
+
+            const data = await response.json();
+
+            if (response.status === 200) {
+                console.log('Registration successful:', data);
+            } else {
+                console.error('Registration failed:', data.error);
+            }
+        } catch (error) {
+            console.error('Request failed', error);
+        }
+    };
+
     return (
         <div className="signUp">
             <div className="signUp__container">
                 <h1>Sign Up</h1>
                 
-                <form>
-                    <h5>Username</h5>
-                    <input type="text" />
+                <form onSubmit={handleSubmit}>
+                    <h5>Name</h5>
+                    <input type="text" value={Imie} onChange={(e) => setImie(e.target.value)}/>
+
+                    <h5>Surname</h5>
+                    <input type="text" value={Nazwisko} onChange={(e) => setNazwisko(e.target.value)}/>
 
                     <h5>Email</h5>
-                    <input type="text" />
+                    <input type="text" value={Email} onChange={(e) => setEmail(e.target.value)}/>
 
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input type="password" value={Haslo} onChange={(e) => setHaslo(e.target.value)}/>
 
-                    <button className="signUp__button">Sign In</button>
+                    <button type="submit" className="signUp__button">Sign In</button>
                 </form>
             </div>
         </div>
