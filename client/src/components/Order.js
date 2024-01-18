a8ef495b009280246efc77d7c7263fda99595840
TUROSINSKI <turosinskipriv@gmail.com>
2024-01-18 15:19:53 +0100

    Creating order actually works

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -1,15 +1,51 @@
 import React from "react";
 import '../styles/Order.css'
+import { useStateValue } from "../StateProvider";
+import { getBasketTotal } from "../reducer";
 
 function Order() {
+
+    const token = localStorage.getItem('token');
+    const [{ basket }, dispatch] = useStateValue();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const CenaKoncowa = getBasketTotal(basket);
+
+        if (!token) {
+            alert("No token found");
+            return;
+        }
+
+        try {
+            const response = await fetch('http://localhost:5000/api/createOrder', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({ CenaKoncowa }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const result = await response.json();
+            console.log("Order created", result);
+        } catch {
+            alert("Error creating the order");
+        }
+    }
+
     return (
         <div className="order">
             <div className="order__container">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <h5 className="order__title">Phone number</h5>
                     <div>
-                    <input className='order__input' type="tel" placeholder="Area code" />
-                    <input className='order__input' type="tel" placeholder="Phone number" />
+                        <input className='order__input' type="tel" placeholder="Area code" />
+                        <input className='order__input' type="tel" placeholder="Phone number" />
                     </div>
 
                     <h5 className="order__title">Shipping Address</h5>
